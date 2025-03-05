5308629cea431eb8cd761e268fcd29dd72b221a2
LeF1nn <k8662232@gmail.com>
2025-03-05 20:01:23 +0330

    explain main

--- Answers/BattleShip/src/main/java/BattleShip.java
+++ Answers/BattleShip/src/main/java/BattleShip.java
@@ -1,5 +1,13 @@
 public class BattleShip {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        int boardSize = 10;
+        char water = '~';
+        char ship = 'S';
+        char hit = 'X';
+        int miss = 'O';
+        int shipNum = 4;
+
+        char[][] view  = Map(boardSize , water ,ship , shipNum);
+
     }
 }
