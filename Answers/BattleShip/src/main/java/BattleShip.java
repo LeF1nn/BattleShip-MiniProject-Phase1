5a910882e1550e94891e26a87c93a57bb038bbb6
LeF1nn <k8662232@gmail.com>
2025-03-06 15:09:23 +0330

    explain main and functions

--- Answers/BattleShip/src/main/java/BattleShip.java
+++ Answers/BattleShip/src/main/java/BattleShip.java
@@ -1,13 +1,23 @@
 public class BattleShip {
     public static void main(String[] args) {
-        int boardSize = 10;
+        int gameBoardLenght = 10;
         char water = '~';
-        char ship = 'S';
+        char ship = 's';
         char hit = 'X';
-        int miss = 'O';
+        char miss = 'O';
         int shipNum = 4;
 
-        char[][] view  = Map(boardSize , water ,ship , shipNum);
 
+        char[][] gameBoard = createGameBoard(gameBoardLenght, water, ship, shipNum);
+        printGameBoard(gameBoard, water, ship);
+        int undetectedShipNum = shipNum;
+        while (undetectedShipNum > 0) {
+            int[] guessCordinates = getUserCordinate(gameBoardLenght);
+            char locatianviewUpdate = evaluateGuessAndGetTheTarget(guessCordinates, gameBoard, ship, water, hit, miss);
+            if (locatianviewUpdate == hit) {
+                undetectedShipNum--;
+            }
+            gameBoard = uptadeGameboard(gameBoard, guessCordinates, locatianviewUpdate);
+            printGameBoard(gameBoard, water, ship);
     }
 }
