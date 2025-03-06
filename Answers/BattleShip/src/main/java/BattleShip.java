44292dd5f9d8b5773023b47c0d9be15c63f4fece
LeF1nn <k8662232@gmail.com>
2025-03-06 20:02:39 +0330

    new(initializeGrid)

--- Answers/BattleShip/src/main/java/BattleShip.java
+++ Answers/BattleShip/src/main/java/BattleShip.java
@@ -1,122 +1,155 @@
-public class BattleShip {
+import java.util.Scanner;
+
+
+public class Main {
+
+    // Grid size for the game
+    static final int GRID_SIZE = 10;
+
+    // Player 1's main grid containing their ships
+    static char[][] player1Grid = new char[GRID_SIZE][GRID_SIZE];
+
+    // Player 2's main grid containing their ships
+    static char[][] player2Grid = new char[GRID_SIZE][GRID_SIZE];
+
+    // Player 1's tracking grid to show their hits and misses
+    static char[][] player1TrackingGrid = new char[GRID_SIZE][GRID_SIZE];
+
+    // Player 2's tracking grid to show their hits and misses
+    static char[][] player2TrackingGrid = new char[GRID_SIZE][GRID_SIZE];
+
+    // Scanner object for user input
+    static Scanner scanner = new Scanner(System.in);
+
+    /**
+     * The main method that runs the game loop.
+     * It initializes the grids for both players, places ships randomly, and manages turns.
+     * The game continues until one player's ships are completely sunk.
+     */
     public static void main(String[] args) {
-        int gameBoardLenght = 10;
-        char water = '~';
-        char ship = 's';
-        char hit = 'X';
-        char miss = 'O';
-        int shipNum = 4;
+        // Initialize grids for both players
+        initializeGrid(player1Grid);
+        initializeGrid(player2Grid);
+        initializeGrid(player1TrackingGrid);
+        initializeGrid(player2TrackingGrid);
 
+        // Place ships randomly on each player's grid
+        placeShips(player1Grid);
+        placeShips(player2Grid);
 
-        char[][] gameBoard = createGameBoard(gameBoardLenght, water, ship, shipNum);
-        printGameBoard(gameBoard, water, ship);
-        int undetectedShipNum = shipNum;
-        while (undetectedShipNum > 0) {
-            int[] guessCordinates = getUserCordinate(gameBoardLenght);
-            char locatianviewUpdate = evaluateGuessAndGetTheTarget(guessCordinates, gameBoard, ship, water, hit, miss);
-            if (locatianviewUpdate == hit) {
-                undetectedShipNum--;
+        // Variable to track whose turn it is
+        boolean player1Turn = true;
+
+        // Main game loop, runs until one player's ships are all sunk
+        while (!isGameOver()) {
+            if (player1Turn) {
+                System.out.println("Player 1's turn:");
+                printGrid(player1TrackingGrid);
+                playerTurn(player2Grid, player1TrackingGrid);
+            } else {
+                System.out.println("Player 2's turn:");
+                printGrid(player2TrackingGrid);
+                playerTurn(player1Grid, player2TrackingGrid);
             }
-            gameBoard = uptadeGameboard(gameBoard, guessCordinates, locatianviewUpdate);
-            printGameBoard(gameBoard, water, ship);
+            player1Turn = !player1Turn;
         }
+
+        System.out.println("Game Over!");
     }
 
-    private static char[][] uptadeGameboard(char[][] gameBoard, int[] guessCordinates, char locatianviewUpdate) {
-        int row = guessCordinates[0];
-        int col = guessCordinates[1];
-        gameBoard[row][col] = locatianviewUpdate;
-        return gameBoard;
-    }
+    /**
+     * Initializes the grid by filling it with water ('~').
+     *
+     * @param grid The grid to initialize.
+     */
+    static void initializeGrid(char[][] grid) {
 
-    private static char evaluateGuessAndGetTheTarget(int[] guessCordinates, char[][] gameBoard, char ship, char water, char hit, char miss) {
-        String messege;
-        int row = guessCordinates[0];
-        int col = guessCordinates[1];
-        char target = gameBoard[row][col];
-        if (target == ship) {
-            messege = "Hit!";
-            target = 'X';
-
-        } else if (target == water) {
-            messege = "Miss";
-            target = 'O';
-
-
-        } else {
-            messege = "Already hit";
-        }
-        System.out.println(messege);
-        return target;
-    }
-
-    private static int[] getUserCordinate(int gameBoardLenght) {
-        int row;
-        int col;
-
-        do {
-            System.out.print("row: ");
-            row = new Scanner(System.in).nextInt();
-        } while (row < 0 || row > +gameBoardLenght + 1);
-        do {
-            System.out.print("col: ");
-            col = new Scanner(System.in).nextInt();
-        } while (col < 0 || col > gameBoardLenght + 1);
-        return new int[]{row, col};
-    }
-
-    private static void printGameBoard(char[][] gameBoard, char water, char ship) {
-        int gameBoardLenght = gameBoard.length;
-        System.out.print(" ");
-        for (int i = 0; i < gameBoardLenght; i++) {
-            System.out.print(i + 1 + " ");
-        }
-        System.out.println();
-        for (int row = 1; row < gameBoardLenght; row++) {
-            System.out.print(row + " ");
-            for (int col = 1; col < gameBoardLenght; col++) {
-                char possition = gameBoard[row][col];
-                if (possition == water) {
-                    System.out.print(water + " ");
-                } else {
-                    System.out.print(possition + " ");
-                }
-
-            }
-            System.out.println();
-        }
-        //System.out.println();
-    }
-
-    private static char[][] createGameBoard(int gameBoardLenght, char water, char ship, int shipNum) {
-        char[][] gameBoard = new char[gameBoardLenght][gameBoardLenght];
-        for (char[] row : gameBoard) {
-            Arrays.fill(row, water);
-        }
-        return placeShips(gameBoard, shipNum, water, ship);
-    }
-
-    private static char[][] placeShips(char[][] gameBoard, int shipNum, char water, char ship) {
-        int placeships = 0;
-        int gameBoardlenght = gameBoard.length;
-        while (placeships < shipNum) {
-            int[] location = generateshipcor(gameBoardlenght);
-
-            char possibleplacement = gameBoard[location[0]][location[1]];
-            if (possibleplacement == water) {
-                gameBoard[location[0]][location[1]] = ship;
-                placeships++;
+        for (int i = 0; i < GRID_SIZE; i++) {
+            for (int j = 0; j < GRID_SIZE; j++) {
+                grid[i][j] = '~';
             }
         }
-        return gameBoard;
     }
 
-    private static int[] generateshipcor(int gameBoardlenght) {
-        int[] cordinate = new int[2];
-        for (int i = 0; i < cordinate.length; i++) {
-            cordinate[i] = new Random().nextInt(gameBoardlenght);
-
-        }
-        return cordinate;
+    /**
+     * Places ships randomly on the given grid.
+     * This method is called for both players to place their ships on their respective grids.
+     *
+     * @param grid The grid where ships need to be placed.
+     */
+    static void placeShips(char[][] grid) {
+        //todo
     }
+
+    /**
+     * Checks if a ship can be placed at the specified location on the grid.
+     * This includes checking the size of the ship, its direction (horizontal or vertical),
+     * and if there's enough space to place it.
+     *
+     * @param grid       The grid where the ship is to be placed.
+     * @param row        The starting row for the ship.
+     * @param col        The starting column for the ship.
+     * @param size       The size of the ship.
+     * @param horizontal The direction of the ship (horizontal or vertical).
+     * @return true if the ship can be placed at the specified location, false otherwise.
+     */
+    static boolean canPlaceShip(char[][] grid, int row, int col, int size, boolean horizontal) {
+        //todo
+        return true;
+    }
+
+    /**
+     * Manages a player's turn, allowing them to attack the opponent's grid
+     * and updates their tracking grid with hits or misses.
+     *
+     * @param opponentGrid The opponent's grid to attack.
+     * @param trackingGrid The player's tracking grid to update.
+     */
+    static void playerTurn(char[][] opponentGrid, char[][] trackingGrid) {
+        //todo
+    }
+
+    /**
+     * Checks if the game is over by verifying if all ships are sunk.
+     *
+     * @return true if the game is over (all ships are sunk), false otherwise.
+     */
+    static boolean isGameOver() {
+        //todo
+        return false;
+    }
+
+    /**
+     * Checks if all ships have been destroyed on a given grid.
+     *
+     * @param grid The grid to check for destroyed ships.
+     * @return true if all ships are sunk, false otherwise.
+     */
+    static boolean allShipsSunk(char[][] grid) {
+        //todo
+        return true;
+    }
+
+    /**
+     * Validates if the user input is in the correct format (e.g., A5).
+     *
+     * @param input The input string to validate.
+     * @return true if the input is in the correct format, false otherwise.
+     */
+    static boolean isValidInput(String input) {
+        //todo
+        return true;
+    }
+
+    /**
+     * Prints the current state of the player's tracking grid.
+     * This method displays the grid, showing hits, misses, and untried locations.
+     *
+     * @param grid The tracking grid to print.
+     */
+    static void printGrid(char[][] grid) {
+        //todo
+    }
+}
+
 
