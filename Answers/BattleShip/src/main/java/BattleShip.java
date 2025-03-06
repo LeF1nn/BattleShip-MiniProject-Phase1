89958bcf124a2a91e14cad7140db7af16b2dfa83
LeF1nn <k8662232@gmail.com>
2025-03-06 15:45:54 +0330

    explain evaluateGuessAndGetTheTarget

--- Answers/BattleShip/src/main/java/BattleShip.java
+++ Answers/BattleShip/src/main/java/BattleShip.java
@@ -19,12 +19,33 @@ public class BattleShip {
             }
             gameBoard = uptadeGameboard(gameBoard, guessCordinates, locatianviewUpdate);
             printGameBoard(gameBoard, water, ship);
+        }
     }
-}
 
     private static char[][] uptadeGameboard(char[][] gameBoard, int[] guessCordinates, char locatianviewUpdate) {
         int row = guessCordinates[0];
         int col = guessCordinates[1];
         gameBoard[row][col] = locatianviewUpdate;
         return gameBoard;
+    }
+
+    private static char evaluateGuessAndGetTheTarget(int[] guessCordinates, char[][] gameBoard, char ship, char water, char hit, char miss) {
+        String messege;
+        int row = guessCordinates[0];
+        int col = guessCordinates[1];
+        char target = gameBoard[row][col];
+        if (target == ship) {
+            messege = "Hit!";
+            target = 'X';
+
+        } else if (target == water) {
+            messege = "Miss";
+            target = 'O';
+
+
+        } else {
+            messege = "Already hit";
+        }
+        System.out.println(messege);
+        return target;
     }
